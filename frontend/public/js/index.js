c16f5b9df1e0d437b0e5250d224845b6072a79f0
Jojancel Daniel <garciagamer81@gmail.com>
2026-03-26 13:00:30 -0400

    validacion del formulario de inicio de sesion

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,4 +1,41 @@
-const click = document.getElementById('click');
-click.addEventListener('click', () => {
-   console.log('Recibido');
+const formulario = document.getElementById('loginForm');
+formulario.addEventListener('submit', function(e) {
+
+    e.preventDefault();
+
+    const usuarioInput = document.getElementById('userName');
+    const contrasenaInput = document.getElementById('contrasena');
+    const usuario = usuarioInput.value.trim();
+    const contrasena = contrasenaInput.value.trim();
+    const errorUsuario = document.getElementById('errorUsuario');
+    const errorContrasena = document.getElementById('errorContrasena');
+
+    errorUsuario.textContent = "";
+    errorContrasena.textContent = "";
+    errorContrasena.classList.remove('mostrar');
+    usuarioInput.classList.remove('input-error');
+    contrasenaInput.classList.remove('input-error');
+    
+    let valido = true;
+
+    if (usuario === "") {
+        errorUsuario.textContent = "El usuario no puede estar vacío";
+        errorUsuario.classList.add('mostrar');
+        usuarioInput.classList.add('input-error', 'vibrar');
+        valido = false;
+    }
+
+    if (contrasena === "") {
+        errorContrasena.textContent = "La contraseña no puede estar vacía";
+        errorContrasena.classList.add('mostrar');
+        contrasenaInput.classList.add('input-error', 'vibrar');
+        valido = false;
+
+    } else if (contrasena.length < 5) {
+        errorContrasena.textContent = "Mínimo 5 caracteres";
+        errorContrasena.classList.add('mostrar');
+        contrasenaInput.classList.add('input-error', 'vibrar');
+        valido = false;
+
+    }
 });
