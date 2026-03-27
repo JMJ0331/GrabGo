a77700bc01e50196a472a638c8006294d1b46abb
Jose Michelle Jimenez <josemichelle0331@gmail.com>
2026-03-27 18:44:56 -0400

    Se agrego varias opciones al controlador para poder registrar y verificar al usuario tanto nuevo como ya existente

--- backend/auth/controllers/auth.registerController.js
+++ backend/auth/controllers/auth.registerController.js
@@ -1,7 +1,53 @@
-import { saveData } from "../models/auth.model.js"
+import { read } from "fs";
+import { readDB, writeDB } from "../models/auth.model.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "dotenv/config";
 
-export function registerController(req, res) {
+config;
+const SECRET = process.env.SECRET || 'grabGo';
+// export function generateToken() {
+
+// }
+
+export async function verifyLoginController(req, res) {
+    const dbAuth = await readDB();
+    const { userName, contrasena } = req.body;
+    const usuarioEncontrado = dbAuth.customers.find(c => c.nombre.split(' ')[0] === userName);
+    if (!usuarioEncontrado) {
+        res.send('no se encontro usuario');
+        return;
+    } else {
+        const seEncontroUsuario = !!(dbAuth.customers && dbAuth.customers.find(c => c.nombre.split(' ')[0] === userName));
+        const usuarioEncontradoClave = usuarioEncontrado.contrasena;
+        const claveCorrecta = await bcrypt.compare(contrasena, usuarioEncontradoClave);
+        if (seEncontroUsuario && claveCorrecta) {
+            const token = jwt.sign({ usuarioEncontrado }, SECRET, { expiresIn: '1m' });
+            res.cookie("token", token, {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: false
+            });
+            res.redirect('/inicio');
+        } else {
+            res.send('Clave incorrecta!');
+        }
+    }
+}
+
+export async function registerController(req, res) {
+    const dbAuth = await readDB();
     const { userName, correo, contrasena } = req.body;
-    const datos = saveData(userName, correo, contrasena);
-    res.send(datos, 'recibidos');
+    const hashContrasena = await bcrypt.hash(contrasena, 10);
+    const newCustomer = {
+        id: dbAuth.customers.length ? dbAuth.customers[dbAuth.customers.length - 1].id + 1 : 1,
+        nombre: userName,
+        correo: correo,
+        contrasena: hashContrasena
+    };
+
+    dbAuth.customers.push(newCustomer);
+    await writeDB(dbAuth);
+    res.redirect('/inicio');
 }
