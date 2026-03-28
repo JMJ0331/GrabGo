242e7e3f0c7c1618ac8a9e46d1d413a64cc331be
jordanibasilio264-afk <jordanibasilio264@gmail.com>
2026-03-28 15:47:37 -0400

    Se creo la estructura temporal de los pagos

--- backend/payments/app.js
+++ backend/payments/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import bcrypt from "bcrypt";
+import fs from 'node:fs';
 import config from "dotenv/config";
 import jwt from "jsonwebtoken";
-import db from './src/db/usuariosRegistrados.json' with { type: 'json' };
+import db from './src/db/metodoPagosRegistrados.json' with { type: 'json' };
 
 config;
 const app = express();
@@ -20,8 +21,31 @@ app.get('/', (req, res) => {
     res.send('Payments');
 });
 
+function prueba() {
+    const quesera = {Usuario: "Manuel", Productos: "Manzanas", precio: "$000000"}
+    return quesera;
+}
+
+app.post('/', (req, res) => {
+    const objeto = path.join(__dirname, "./src/db/metodoPagosRegistrados.json")
+    const nuevo = {payments: []}
+    if (fs.existsSync(objeto)) {
+        console.log("Se encontro el archivo")
+            const contenido = fs.readFileSync(objeto, "utf-8")
+            try {
+                const jsonleido = JSON.parse(contenido)
+                if (jsonleido && jsonleido.payments) {
+                    nuevo.payments = jsonleido.payments
+                }
+                  } catch (e) {
+                console.log("JSON corrupto, reseteando a lista vacía");
+            }}
+    nuevo.payments.push(prueba())
+    fs.writeFileSync('./src/db/metodoPagosRegistrados.json', JSON.stringify(nuevo, null, 2), "utf-8")
+    console.log("Archivo guardado con éxito");
+    res.send(`Datos recibidos correctamente`);
+})
+
 app.listen(port, () => {
     console.log(`Payments esta ejecutandose... http://localhost:${port}`);
 });
-
-console.log(db);
